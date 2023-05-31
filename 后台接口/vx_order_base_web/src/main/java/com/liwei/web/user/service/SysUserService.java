ec220ba20fc1c5a7ed5064789b1d20ab5a3d4dd2
李玮 <505805473@qq.com>
2023-05-31 16:53:27 +0800

    feat: 梳理整合

--- 后台接口/vx_order_base_web/src/main/java/com/liwei/web/user/service/SysUserService.java
+++ 后台接口/vx_order_base_web/src/main/java/com/liwei/web/user/service/SysUserService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.liwei.web.user.entity.SysUser;
 
 public interface SysUserService extends IService<SysUser> {
+     SysUser login(String userName, String password);
+
 }
